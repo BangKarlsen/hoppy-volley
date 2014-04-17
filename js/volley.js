ef4fa71757004ee9c947658bce578bea55a5be68
bangkarlsen <jhpe04@gmail.com>
2014-04-17 19:36:03 +0200

    player refactoed

--- js/volley.js
+++ js/volley.js
@@ -1,13 +1,54 @@
 (function() {
 
-    var player;
+    var player1;
     var platforms;
     var cursors;
     var ball;
     var debugBallCollision;
 
-    var Player = function() {
+    var Player = function(playerName, startPosX) {
+        var name = playerName;
+        var sprite = game.add.sprite(startPosX, game.world.height - 35, 'dude');
+        game.physics.p2.enable(sprite, false);
+        sprite.body.fixedRotation = true;
+        sprite.body.onBeginContact.add(playerHit, this);
 
+        function checkIfCanJump() {
+            var yAxis = p2.vec2.fromValues(0, 1);
+            var result = false;
+
+            for (var i = 0; i < game.physics.p2.world.narrowphase.contactEquations.length; i++) {
+                var c = game.physics.p2.world.narrowphase.contactEquations[i];
+
+                if (c.bi === player1.sprite.body.data || c.bj === player1.sprite.body.data) {
+                    var d = p2.vec2.dot(c.ni, yAxis); // Normal dot Y-axis
+                    if (c.bi === player1.sprite.body.data) {
+                        d *= -1;
+                    }
+                    if (d > 0.5) result = true;
+                }
+            }
+            return result;
+        }
+
+        return {
+            name: name,
+            sprite: sprite,
+            canJump: checkIfCanJump
+        };
+    }
+
+
+    var Ball = function() {
+        var sprite = game.add.sprite(35, game.world.height / 2, 'ball');
+        game.physics.p2.enable(sprite, false);
+        sprite.body.setCircle(sprite.width / 2);
+        sprite.body.mass = 0.3;
+        sprite.body.gravity.setMagnitude(0);
+
+        return {
+            sprite: sprite
+        };
     }
 
     var game = new Phaser.Game(800, 400, Phaser.AUTO, 'volley', {
@@ -27,26 +68,24 @@
     function create() {
         //  A simple background for our game
         game.add.sprite(0, 0, 'sky');
-        // The player and the ball
-        player = game.add.sprite(32, game.world.height - 35, 'dude');
-        ball = game.add.sprite(35, game.world.height / 2, 'ball');
 
         //  We're going to be using physics, so enable the Physics system
         game.physics.startSystem(Phaser.Physics.P2JS);
         game.physics.p2.defaultRestitution = 0.9;
         game.physics.p2.gravity.y = 800;
 
-        //  We need to enable physics on all sprites
-        game.physics.p2.enable([player, ball], false);
-        ball.body.setCircle(ball.width / 2);
-        ball.body.mass = 0.3;
-        ball.body.gravity.setMagnitude(0);
+        // Create the ball and the players
+        player1 = new Player('Bobby', 32);
+        ball = new Ball();
 
-        player.body.fixedRotation = true;
-        player.body.onBeginContact.add(playerHit, this);
+        initAllMaterials();
 
-        var playerMaterial = game.physics.p2.createMaterial('playerMaterial', player.body);
-        var ballMaterial = game.physics.p2.createMaterial('ballMaterial', ball.body);
+        cursors = game.input.keyboard.createCursorKeys();
+    }
+
+    function initAllMaterials() {
+        var playerMaterial = game.physics.p2.createMaterial('playerMaterial', player1.sprite.body);
+        var ballMaterial = game.physics.p2.createMaterial('ballMaterial', ball.sprite.body);
         var worldMaterial = game.physics.p2.createMaterial('worldMaterial');
 
         //  4 trues = the 4 faces of the world in left, right, top, bottom order
@@ -64,7 +103,6 @@
         playerBallContactMaterial.friction = 0.9; // Friction to use in the contact of these two materials.
         playerBallContactMaterial.restitution = 1.2; // Restitution (i.e. how bouncy it is!) to use in the contact of these two materials.
 
-        cursors = game.input.keyboard.createCursorKeys();
     }
 
     function playerHit(body, shapeA, shapeB, equation) {
@@ -72,40 +110,23 @@
     }
 
     function update() {
-        player.body.setZeroForce();
+        player1.sprite.body.setZeroForce();
 
         if (cursors.left.isDown) {
-            player.body.moveLeft(250);
+            player1.sprite.body.moveLeft(250);
         } else if (cursors.right.isDown) {
-            player.body.moveRight(250);
+            player1.sprite.body.moveRight(250);
         }
         //  Allow the player to jump if they are touching the ground.
-        if (cursors.up.isDown && checkIfCanJump()) {
-            player.body.moveUp(500);
+        if (cursors.up.isDown && player1.canJump()) {
+            player1.sprite.body.moveUp(500);
         }
     }
 
-    function checkIfCanJump() {
-        var yAxis = p2.vec2.fromValues(0, 1);
-        var result = false;
-
-        for (var i = 0; i < game.physics.p2.world.narrowphase.contactEquations.length; i++) {
-            var c = game.physics.p2.world.narrowphase.contactEquations[i];
-
-            if (c.bi === player.body.data || c.bj === player.body.data) {
-                var d = p2.vec2.dot(c.ni, yAxis); // Normal dot Y-axis
-                if (c.bi === player.body.data) {
-                    d *= -1;
-                }
-                if (d > 0.5) result = true;
-            }
-        }
-        return result;
-    }
 
     function render() {
         game.debug.text(debugBallCollision, 32, 32);
-        game.debug.text('Ball mass:' + ball.body.mass, 32, 32 * 2);
+        game.debug.text('Player1: ' + player1.name, 32, 32 * 2);
     }
 
 }());
