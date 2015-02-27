ff9e9a4ead21f400a2a73f3fef123538deac407e
bangkarlsen <jhpe04@gmail.com>
2015-02-27 14:42:29 +0100

    Move settings to seperate file

--- js/game.js
+++ js/game.js
@@ -1,8 +1,8 @@
 'use strict';
 
 define([
-    'phaser', 'ball', 'court', 'player'
-], function (Phaser, Ball, Court, Player) {
+    'phaser', 'ball', 'court', 'player', 'settings'
+], function (Phaser, Ball, Court, Player, Settings) {
     function Game() {
         console.log('Making the Game');
     }
@@ -17,19 +17,7 @@ define([
             render: this.render
         });
 
-        this.game.settings = {
-            debug: false,
-            jumpForce: 450,
-            moveForce: 200,
-            ballWorldFriction: 0.1, // Friction in the contact of these two materials.
-            ballWorldRestitution: 0.9, // how bouncy it is!
-            playerWorldFriction: 0.5,
-            playerWorldRestitution: 0.3,
-            playerBallFriction: 0.9,
-            playerBallRestitution: 1.3,
-            playerCourtFriction: 0.5,
-            playerCourtRestitution: 0.3
-        };
+        this.game.settings = Settings; 
     };
 
     Game.prototype.preload = function () {
