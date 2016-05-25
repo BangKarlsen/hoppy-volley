89430e8146b01c777a99f77ffe34f6bf2e5fe5a7
bangkarlsen <jhpe04@gmail.com>
2016-05-25 23:08:31 +0200

    Add randomness to the serve

--- js/ai.js
+++ js/ai.js
@@ -11,12 +11,15 @@ define(function() {
         return Math.sqrt((pos2.x - pos1.x) * (pos2.x - pos1.x) + (pos2.y - pos1.y) * (pos2.y - pos1.y));
     }
 
-    function doServe(player, ball) {
+    function randRange(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    function doServe(player, ball, minServeDistance) {
         var commands = [];
+        
         if (player.side === 'left') {
-            console.log('dist ' + distance(player.pos(), ball.pos()));
-            if (distance(player.pos(), ball.pos()) < 95) {
-                console.log('jmp!');
+            if (distance(player.pos(), ball.pos()) < minServeDistance) {
                 commands.push(player.jump.bind(player));
             } else {
                 commands.push(player.moveRight.bind(player));
@@ -36,7 +39,8 @@ define(function() {
 
         if (!ball.isActive) {
             if (ball.lastServer === player.id) {
-                return doServe(player, ball);
+                // return doServe(player, ball, randRange(96, 110)); // some mistakes
+                return doServe(player, ball, randRange(97, 103));  // only good serves
             }
         }
 
